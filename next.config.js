78435d118568c6e7ebfa66ae7da0bab2e0b9d93b
alihaiderdev <ah1675179@gmail.com>
2021-10-21 04:50:28 +0500

    another way of link href value setting and customizations in next.config file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,46 @@
 module.exports = {
   reactStrictMode: true,
-}
+};
+
+// whenever we change in this file we must start server and make new build otherwise changing will not be reflected
+// module.exports = {
+//   reactStrictMode: true,
+//   env: {
+//     mySiteUrl: 'www.google.com',
+//   },
+//   // basePath: '/codeText', // by setting basePath to this now we can not access our al urls like before e.g: localhost:3000/home this will show an error after doing this instead localhost:3000/codeText/home
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/testing',
+//         destination: '/blog/2',
+//       },
+//       {
+//         source: '/dummy/:path',
+//         destination: '/info/:path',
+//       },
+//     ];
+//   },
+//   async redirects() {
+//     return [
+//       {
+//         source: '/mytesting',
+//         destination: '/blog/5',
+//         permanent: true,
+//       },
+//     ];
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: '/blog/:blogId',
+//         headers: [
+//           {
+//             key: 'xyz-code-imrove',
+//             value: 'yes code imrove :blogId',
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
--- pages/home.js
+++ pages/home.js
@@ -27,7 +27,12 @@ const Home = () => {
   });
   return (
     <div className='home'>
-      <h1>This is home page</h1>
+      <h1>This is home page</h1>{' '}
+      <a
+        href={`${process.env.mySiteUrl}`}
+        target='_blank'
+        rel='noreferrer'
+      >{`${process.env.mySiteUrl}`}</a>
       <button onClick={() => router.back()}>Back</button>
       {/* replace function bhi back ki tarha hi work karta hai but back hamain us route pa la kar chae jata hai jahan sa hum ate hain but replace hum jis route par sa ata hain us sa aak step piche wale route pa la jata hai or ya hamara multi steps form wale scenarios ma kaam asakta hai  */}
       <button onClick={() => router.replace('/blog/6')}>Replace Blog 6</button>
@@ -49,30 +54,51 @@ const Home = () => {
         </button>
       </div>
       <div>
-        <Link href={{ pathname: '/about', query: { name: 'ali', age: 23, gender: 'male', employeeAtAnyCompany: true } }}>
+        <Link
+          href={{
+            pathname: '/about',
+            query: { name: 'ali', age: 23, gender: 'male', employeeAtAnyCompany: true },
+          }}
+        >
           <a>
             <button>About query</button>
           </a>
         </Link>
       </div>
-
       {/* we use passHref to passing link href value to its direct child if its direct child is a tag then we dont use it by deauld its paas to it  */}
       <div>
-        <Link passHref href={{ pathname: '/about', query: { name: 'ali', age: 23, gender: 'male', employeeAtAnyCompany: true } }}>
+        <Link
+          passHref
+          href={{
+            pathname: '/about',
+            query: { name: 'ali', age: 23, gender: 'male', employeeAtAnyCompany: true },
+          }}
+        >
           <button>About query using passRef</button>
         </Link>
       </div>
-
       {/* use of passHref  */}
-      <Link passHref href={{ pathname: '/about', query: { name: 'ali', age: 23, gender: 'male', employeeAtAnyCompany: true } }}>
+      <Link
+        passHref
+        href={{
+          pathname: '/about',
+          query: { name: 'ali', age: 23, gender: 'male', employeeAtAnyCompany: true },
+        }}
+      >
         <Button />
       </Link>
       <div>
         {/* <button onClick={() => router.push('/news')}>News</button> */}
         <button onClick={redirect}>News</button>
-        <button onClick={() => router.push('/home?page=1', undefined, { shallow: true })}>Home 1</button>
-        <button onClick={() => router.push('/home?page=2', undefined, { shallow: true })}>Home 2</button>
-        <button onClick={() => router.push('/home?page=3', '/about', { shallow: true })}>Home 3</button>
+        <button onClick={() => router.push('/home?page=1', undefined, { shallow: true })}>
+          Home 1
+        </button>
+        <button onClick={() => router.push('/home?page=2', undefined, { shallow: true })}>
+          Home 2
+        </button>
+        <button onClick={() => router.push('/home?page=3', '/about', { shallow: true })}>
+          Home 3
+        </button>
       </div>
     </div>
   );
