1af0db90b4be87a1de9d579f0b4efdbe4ba69ba6
alihaiderdev <ah1675179@gmail.com>
2021-10-20 12:53:09 +0500

    styling custom and global

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import styles from '../styles/Home.module.css';
+import customStyle from '../styles/custom.module.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  //   return <div className={styles.footer}>Header</div>;
+  return <div className={`${styles.footer} ${customStyle.color}`}>Header</div>;
 };
 
 export default Header;
