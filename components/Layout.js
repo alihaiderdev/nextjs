fdcc6e954cc4c38e0e44b4d00867f1563274ec81
alihaiderdev <ah1675179@gmail.com>
2021-10-20 12:20:29 +0500

    Common Layout

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+
+const Layout = ({ children }) => {
+  return (
+    <div className='layout'>
+      <Header />
+      <h1>This is common layout</h1>
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
